58613192d9ea6d915de72651163b26d1b82f608f
Iuriy <webacademic@yandex.ru>
2022-04-05 19:03:36 +0300

    translate

--- src/blocks/modules/translate/dictionary.js
+++ src/blocks/modules/translate/dictionary.js
@@ -100,16 +100,216 @@ const langArr = {
         "ru": "30д"
     },
     // tokenomics
-    "tokenomics-token": {
-        "en": "Управляющий токен",
+    "tokenomics-title": {
+        "en": "Tockenomic project",
+        "ru": "ТОКЕНОМИКА ПРОЕКТА"
+    },
+    "tokenomics-label": {
+        "en": "Basic concepts of Tockenomics of the Universe Metacraft",
+        "ru": "Основные понятия токеномики вселенной METACRAFT"
+    },
+    "tokenomics-name-1": {
+        "en": "Governance platform token",
         "ru": "Управляющий токен"
     },
-    "tokenomics-token-about": {
-        "en": "Управляющий (governance) токен платформы, предназначенный для инвестиционных и управленческих целей.",
+    "tokenomics-descr-1-1": {
+        "en": "Governance platform token. Meant for investment and management purposes",
         "ru": "Управляющий (governance) токен платформы, предназначенный для инвестиционных и управленческих целей."
     },
-
-
+    "tokenomics-name-2": {
+        "en": "CraftUSD Stable Token",
+        "ru": "Стейбл-токен CraftUSD"
+    },
+    "tokenomics-descr-2-1": {
+        "en": "cUSD consists of tokenized gold (PAXG) and Ethereum (ETH) in approximately equal shares (50/50%) at the start of the project.",
+        "ru": "Обеспечен фондом CraftFund, состоящий из токенезированного золота (PAXG) и эфира (ETH), приблизительно в равных долях (50/50%) на старте проекта."
+    },
+    "tokenomics-descr-2-2": {
+        "en": "Stabletoken cUSD will be rewarded to users, project partners and the project team.",
+        "ru": "Стейблтокеном cUSD будут вознаграждаться пользователи, партнеры проекта и команда проекта."
+    },
+    "tokenomics-name-3": {
+        "en": "Radiant token",
+        "ru": "Токен Radiant"
+    },
+    "tokenomics-descr-3-1": {
+        "en": "Token share of the total game prize pool. RAD token holders can claim % of the total prize pool at the moment of unlocking, depending on the number of tokens. The number of RAD tokens is limited (1M).",
+        "ru": "Токен-доля общего игрового фонда. Обладатели токена RAD могут претендовать на % от общего призового пула в момент раскрытия в зависимости от кол-ва токенов. Количество токенов RAD ограничено (1M)."
+    },
+    "tokenomics-descr-3-2": {
+        "en": "Up to 85% of all sales and services go to the general game fund. Thus, the value of the RAD token will grow in proportion to the liquidity in the fund.",
+        "ru": "До 85% всех продаж и услуг сервиса направляется в общий игровой фонд, таким образом, стоимость токена RAD будет расти пропорционально объему ликвидности в фонде."
+    },
+    "tokenomics-name-4": {
+        "en": "Total game fund",
+        "ru": "Общий игровой фонд"
+    },
+    "tokenomics-descr-4-1": {
+        "en": "Total game fund (CraftFund), provides all objects and mechanics of the meta-world, consists of tokenized gold (PAXG) and ether (ETH) in a 50/50% proportion. CraftFund is permanently filled with all the sales of facilities and services in the meta-world.",
+        "ru": "Обеспечивающий все объекты и механики мета-мира, состоящий из токенезированного золота (PAXG) и эфира (ETH) в пропорции 50/50%. Игровой фонд CraftFund постоянно пополняется со всех продаж объектов и услуг в мета-мире."
+    },
+    "tokenomics-descr-4-2": {
+        "en": "Also, the volume of the fund will grow in the mid-term due to an increase of number and value of supporting assets (PAXG/ETH). ",
+        "ru": "Также объем фонда в среднесрочной перспективе будет расти за счет увеличения количества и роста стоимости обеспечивающих активов (PAXG/ETH)."
+    },
+    "tokenomics-descr-5-1": {
+        "en": "The mechanisms of issue, distribution, and pricing of cUSD, RAD, and NCT will maximize encourage the creation of sustainable inner and outer value in the METACRAFT world.",
+        "ru": "Механизмы эмиссии, циркуляции и ценообразования для cUSD, RAD, NCT будут максимально поощрять создание устойчивой внутренней и внешней ценности в мире METACRAFT."
+    },
+    "tokenomics-descr-5-2": {
+        "en": "The main game fund will unlock at the price of ETH more than 15K USD. This model will allow high profits from the activity and development of game objects to almost all participants of the project.",
+        "ru": "Главный игровой фонд раскроется при цене ETH более 15K USD. Данная модель позволит получить высокую прибыль от активности и развития игровых объектов почти всем участникам проекта."
+    },
+    "tokenomics-subtitle-1": {
+        "en": "HOW PLAYERS EARN",
+        "ru": "КАК ЗАРАБАТЫВАЮТ <br> ИГРОКИ"
+    },
+    "tokenomics-token-descr-1-1": {
+        "en": "Players, by completing tasks, participating in tournaments, building architectural structures, and mining valuable resources, receive cUSD - game stable token in the amount from 1/1000 to 1/50 of the current fund size.",
+        "ru": "Игроки, выполняя задания, участвуя в турнирах, строя архитектурные сооружения и добывая ценные ресурсы, получают cUSD - игровой стейблтокен в размере от 1/1000 до 1/50 от текущего размера фонда."
+    },
+    "tokenomics-token-descr-1-2": {
+        "en": "You can withdraw cUSD over time or buy with it: RAD prize pool tokens, NCT platform tokens, NFT objects, services, or other opportunities on the platform and worlds based on it.",
+        "ru": "cUSD можно вывести в полном объеме с течением времени или купить на всю сумму на выбор: токены доли призового пула RAD, токены платформы NCT, NFT-объекты, услуги или другие возможности на платформе и мирах, созданных на ее основе."
+    },
+    "tokenomics-subtitle-2": {
+        "en": "HOW THE TEAM EARNS",
+        "ru": "КАК ЗАРАБАТЫВАЕТ <br> КОМАНДА"
+    },
+    "tokenomics-token-descr-2-1": {
+        "en": "The team earns through transaction fees of up to 15% and a share of the CraftFund's total game fund, 20%, at the time of unlock",
+        "ru": "Команда зарабатывает за счет комиссий с транзакций до 15% и доли от общего игрового фонда CraftFund в момент раскрытия в размере 20%."
+    },
+    "tokenomics-subtitle-3": {
+        "en": "HOW INVESTORS EARN MONEY",
+        "ru": "КАК ЗАРАБАТЫВАЮТ <br> ИНВЕСТОРЫ"
+    },
+    "tokenomics-token-descr-3-1": {
+        "en": "Investors can earn money by buying NCT tokens or prize pool - RAD tokens",
+        "ru": "Инвесторы могут заработать покупая токены проекта - NCT или токены призового пула - RAD"
+    },
+    "tokenomics-token-descr-3-2": {
+        "en": "The value of the control NCT token inevitably increases when the planned operational results of the project are achieved.",
+        "ru": "Стоимость управляющего токена NCT неизбежно увеличивается, при достижении планируемых операционных показателей проекта."
+    },
+    "tokenomics-token-descr-3-3": {
+        "en": "The more RAD tokens an investor bought the higher % of the total prize pool and, accordingly, the share of the reward will be received. At the unlock time of CraftFund.",
+        "ru": "Чем больше инвестор купил токенов RAD, тем больше % от общего призового пула и соответственно доля вознаграждения, в момент раскрытия CraftFund."
+    },
+    //achievements
+    "achievements-title": {
+        "en": "ACHIEVEMENTS",
+        "ru": "Результаты"
+    },
+    "achievements-name-1": {
+        "en": "Сreated prototype of METACRAFT in Minecraft world",
+        "ru": "Создали прототип метавселенной METACRAFT в мире Minecraft"
+    },
+    "achievements-name-2": {
+        "en": "Received funding of $3.7M USD",
+        "ru": "Получили финансирование в размере $3,7M USD"
+    },
+    "achievements-name-3": {
+        "en": "Regular tournaments in Minecraft 3D universe",
+        "ru": "Регулярные турниры в 3D-метавселенной Minecraft"
+    },
+    "achievements-name-4": {
+        "en": "Assembled a top team of experts and developers",
+        "ru": "Собрали ТОП-команду экспертов и разработчиков"
+    },
+    "achievements-name-5": {
+        "en": "Browser game on blockchain",
+        "ru": "Браузерная игра на блокчейне"
+    },
+    "achievements-name-6": {
+        "en": "Sold over 1,000 NFTs and created a sustainable economy",
+        "ru": "Продали более 1.000 NFT и создали устойчивую экономику"
+    },
+    "achievements-name-7": {
+        "en": "Multiplatform 3D game MVP on Unity",
+        "ru": "MVP мультиплатформенной 3D-игры на Unity "
+    },
+    "achievements-name-8": {
+        "en": "Successfully performed presentations and mitaps, received positive feedback",
+        "ru": "Провели ряд презентаций и митапов, получили положительную оценку"
+    },
+    "achievements-name-9": {
+        "en": "Released of Governance-token NCT",
+        "ru": "Выпуск Governance-токена NCT"
+    },
+    "achievements-name-10": {
+        "en": "Partnerships with large funds and strong partners",
+        "ru": "Заключили партнерства с крупными фондами и сильными партнерами"
+    },
+    //numbers
+    "numbers-title": {
+        "en": "NFTCRAFT IN NUMBERS",
+        "ru": "NFTCRAFT В ЦИФРАХ"
+    },
+    "numbers-item-label-1": {
+        "en": "Sales:",
+        "ru": "Продажи:"
+    },
+    "numbers-item-descr-1": {
+        "en": "sold >1000 NFT objects",
+        "ru": "продано >1000 NFT объектов"
+    },
+    "numbers-item-label-2": {
+        "en": "Partners:",
+        "ru": "Партнеры:"
+    },
+    "numbers-item-descr-2": {
+        "en": ">10 major partners OwnFund, Envelop, Venus etc.",
+        "ru": ">10 крупных партнеров OwnFund, Envelop, Venus etc."
+    },
+    "numbers-item-label-3": {
+        "en": "Инвестиции:",
+        "ru": "Marketing company:"
+    },
+    "numbers-item-descr-3": {
+        "en": "500K USDT monthly from 05.2022",
+        "ru": "500K USDT ежемесячно с 05.2022"
+    },
+    "numbers-item-label-4": {
+        "en": "GOAL:",
+        "ru": "Цель:"
+    },
+    "numbers-item-descr-4": {
+        "en": ">300K users in 4Q.2022",
+        "ru": ">300K пользователей в 4Q.2022"
+    },
+    "numbers-item-label-5": {
+        "en": "Team:",
+        "ru": "Команда:"
+    },
+    "numbers-item-descr-5": {
+        "en": "35 people",
+        "ru": "35 человек"
+    },
+    "numbers-item-label-6": {
+        "en": "Community:",
+        "ru": "Коммьюнити:"
+    },
+    "numbers-item-descr-6": {
+        "en": ">5000 active participants",
+        "ru": ">5000 участников"
+    },
+    "numbers-item-label-7": {
+        "en": "Game events:",
+        "ru": "Игровые ивенты:"
+    },
+    "numbers-item-descr-7": {
+        "en": "Held >20 events PvE / Build / 2D",
+        "ru": "проведено >20 ивентов PvE / Стройка / 2D"
+    },
+    "numbers-item-label-8": {
+        "en": "Investments:",
+        "ru": "Инвестиции:"
+    },
+    "numbers-item-descr-8": {
+        "en": "3.7M USD raised",
+        "ru": "привлечено 3,7M USD"
+    },
 }
 
 export { langArr };
