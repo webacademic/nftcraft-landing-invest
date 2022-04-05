fe4f3b019c7e164d2d9cc1038059272a8041cd3b
Iuriy <webacademic@yandex.ru>
2022-04-05 10:34:51 +0300

    init

---
+++ src/blocks/modules/header/header.js
@@ -0,0 +1,27 @@
+let e = document.querySelector(".mobile-menu__btn"),
+  overlay = document.querySelector(".overlay"),
+  t = document.querySelector(".header");
+
+if (e) {
+    e.addEventListener("click", function () {
+        t.classList.toggle("mobile");
+        overlay.classList.toggle("active");
+        if (t.classList.contains("mobile")) {
+          document.querySelector("body").style.overflow = "hidden";
+        } else {
+          document.querySelector("body").style.overflow = "visible";
+        }
+      });
+}
+if (overlay) {
+    overlay.addEventListener("click", function () {
+        this.classList.remove("active");
+        document.querySelector(".mobile-menu__checkbox").checked = !1;
+        t.classList.remove("mobile");
+        if (t.classList.contains("mobile")) {
+          document.querySelector("body").style.overflow = "hidden";
+        } else {
+          document.querySelector("body").style.overflow = "visible";
+        }
+      });
+}
